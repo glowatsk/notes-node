0f4b5e6d9d488728f204018b5d9e06205e2cd011
glowatsk <glowatsk@gmail.com>
2018-04-05 12:56:59 -0600

    Added read note function

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var command = process.argv[2];
 if (command === 'add'){
     var note = notes.addNote(argv.title, argv.body);
     if (note){
-        console.log('------Note Created-------')
-        console.log(`Title: ${note.title} Body: ${note.body}`);
-        console.log('-------------------------')
+        notes.logNote(note);
     }
     else{
         console.log('Note title already exists');
@@ -23,9 +21,17 @@ if (command === 'add'){
 } else if (command === 'list'){
     notes.getAll();
 } else if (command === 'read'){
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note){
+        notes.logNote(note);
+    } else {
+        console.log('Note not found');
+    }
+
 } else if (command === 'remove'){
-    notes.removeNote(argv.title);
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed.' : 'Note was not found.';
+    console.log(message);
 } else{
     console.log('Command not recognized');
 }
--- note.js
+++ note.js
@@ -38,19 +38,29 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Reading Note: ' + title);
-
+    var notes = fetchNotes();
+    filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 };
 
 var removeNote = (title) => {
     var notes = fetchNotes();
     filteredNotes = notes.filter((note) => note.title !== title);
     saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+    console.log('---------------')
+    console.log(`Title: ${note.title}`)
+    console.log(`Body: ${note.body}`)
+    console.log('---------------')
 };
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
